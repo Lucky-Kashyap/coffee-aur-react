21eeb2827cbdbf8d984ba3b792241b6835f48d60
Lucky <luckykashyap3430@gmail.com>
2023-10-05 11:49:00 +0530

    create react projects using create-react & vite utility

---
+++ basic-react/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1>Coffee aur react with creat-reat-app | Divyanshu </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ basic-vite/main.js
@@ -0,0 +1,6 @@
+document.querySelector('#app').innerHTML = `
+  <div>
+   <h1>coffee aur react with vite | Divyanshu </h1>
+  </div>`
+
+
