7f80211c991e96c22efeaca6ad96dea6a427ae82
Lucky <luckykashyap3430@gmail.com>
2023-10-05 16:17:42 +0530

    custom react create

--- basic-vite/src/App.jsx
+++ basic-vite/src/App.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import Chai from "./chai.jsx";
 
 function App(){
+    const username = 'coffee aur code';
+
     return (
         <>
         <h1>coffee aur react with vite | Divyanshu </h1>
+        <p>{username}</p>
         <Chai/>
         </>
     )
--- basic-vite/src/main.jsx
+++ basic-vite/src/main.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+// import {jsx as _jsx} from "react/jsx-runtime.js";
 
 // document.querySelector('#app').innerHTML = `
 //   <div>
@@ -9,11 +10,46 @@ import App from "./App.jsx";
 //   <App />
 //   </div>`
 
+function MyApp() {
+  return (
+    <div>
+      <h3>Custom React ...!</h3>
+    </div>
+  );
+}
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// const reactElement = {
+//   type: "a",
+//   props: {
+//     href: "https://google.com",
+//     target: "_blank",
+//   },
+//   children: "Click me to visit google",
+// };
 
+const anotherElement = (
+  <a href="https://www.google.com" target="_blank">
+    Click me to open google
+  </a>
+);
 
+const anotherUser = "Divyanshu";
+
+const reactElement = React.createElement(
+  "a",
+  {
+    target: "_blank",
+    href: "https://www.facebook.com",
+  },
+  "click me to visit facebook",
+  anotherUser
+);
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+
+// reactElement,
+// )
+
+ReactDOM.createRoot(document.getElementById("root")).render(reactElement);
+
+// ReactDOM.createRoot(document.getElementById("root")).render(<App/>);
