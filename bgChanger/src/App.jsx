388ca1ebb21ad9f6453d69d37b91c60d73a7d572
Lucky <luckykashyap3430@gmail.com>
2023-10-07 11:25:06 +0530

    add random button

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -22,10 +22,10 @@ function App() {
 
 <h1 className='text-center text-white font-bold py-4'>BackGround Color : {change}</h1> <br /> <br />
      <div className='fixed flex flex-wrap justify-content gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
-<button onClick={generateRandom} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor:'red'}}>RED</button>
-<button onClick={generateRandom}  className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor:'green'}}>GREEN</button>
+<button onClick={generateRandom} className='text-center outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor:'red'}}>Click on this to generate Random Color </button>
+{/* <button onClick={generateRandom}  className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor:'green'}}>GREEN</button> */}
 
-<button onClick={generateRandom}  className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor:'orange'}}>ORANGE</button>
+{/* <button onClick={generateRandom}  className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor:'orange'}}>ORANGE</button> */}
 
      </div>
 
