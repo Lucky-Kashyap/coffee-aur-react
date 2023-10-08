f27d94f10bef7b4f9a69ffb74ca5e42e8c4de2be
Lucky <luckykashyap3430@gmail.com>
2023-10-08 17:56:41 +0530

    simple counter app

--- practice/taskmate/src/App.js
+++ practice/taskmate/src/App.js
@@ -1,19 +1,37 @@
 import "./App.css";
-import React from "react";
-import Header from "./components/Header";
-import Card from "./components/Card";
-import Footer from "./components/Footer";
+import React, { useState } from "react";
+// import Header from "./components/Header";
+// import Card from "./components/Card";
+// import Footer from "./components/Footer";
 
 function App() {
+  // let count=0;
+  const [count,setCount] = useState(0);
 
-  const username = "Divyanshu Kashyap";
+  // const username = "Divyanshu Kashyap";
 
+  const add = ()=>{
+    // count +=1;
+    setCount(count+1);
+    console.log(count);
+  }
+  const sub = ()=>{
+    
+    setCount(count-1);
+    console.log(count);
+  }
   return (
-    <div>
-      <h1>Create react app </h1>
-      <Header/>
+    <div className="app">
+      {/* <h1>Create react app </h1> */}
+      <div className="box">
+        <p>{count}</p>
+        <button onClick={add} className="add">Add</button>
+        <button onClick={sub} className="sub">Sub</button>
+      </div>
+
+      {/* <Header/>
       <Card/>
-      <Footer/>
+      <Footer/> */}
       {/* {React.createElement('h2',{},'Heading2')}
       <h3>Created By : {username}</h3> */}
     </div>
