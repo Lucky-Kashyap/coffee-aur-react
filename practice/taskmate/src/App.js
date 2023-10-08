809d6794f14a9fe601ee115e9a9aad165c40fdd1
Lucky <luckykashyap3430@gmail.com>
2023-10-08 15:23:12 +0530

    create variable

--- practice/taskmate/src/App.js
+++ practice/taskmate/src/App.js
@@ -2,10 +2,14 @@ import "./App.css";
 import React from "react";
 
 function App() {
+
+  const username = "Divyanshu Kashyap";
+
   return (
     <div>
-      <h1>Create react app</h1>
+      <h1>Create react app </h1>
       {React.createElement('h2',{},'Heading2')}
+      <h3>Created By : {username}</h3>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus nobis ad saepe eum! Ipsa amet nobis soluta. Ea aspernatur, culpa repellendus iste, atque optio, saepe laboriosam tempore sed quo non.</p>
       <img width={'400px'} src="https://images.unsplash.com/photo-1576248004793-2c21b24b7bcc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80" alt="" />
     </div>
