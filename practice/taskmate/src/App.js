7d1ffd52d3d4220b87827700b50edd7e398b08cf
Lucky <luckykashyap3430@gmail.com>
2023-10-08 15:33:11 +0530

    use components

--- practice/taskmate/src/App.js
+++ practice/taskmate/src/App.js
@@ -1,5 +1,8 @@
 import "./App.css";
 import React from "react";
+import Header from "./components/Header";
+import Card from "./components/Card";
+import Footer from "./components/Footer";
 
 function App() {
 
@@ -8,10 +11,11 @@ function App() {
   return (
     <div>
       <h1>Create react app </h1>
-      {React.createElement('h2',{},'Heading2')}
-      <h3>Created By : {username}</h3>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus nobis ad saepe eum! Ipsa amet nobis soluta. Ea aspernatur, culpa repellendus iste, atque optio, saepe laboriosam tempore sed quo non.</p>
-      <img width={'400px'} src="https://images.unsplash.com/photo-1576248004793-2c21b24b7bcc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80" alt="" />
+      <Header/>
+      <Card/>
+      <Footer/>
+      {/* {React.createElement('h2',{},'Heading2')}
+      <h3>Created By : {username}</h3> */}
     </div>
   );
 }
---
+++ practice/taskmate/src/components/Card.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div>
+        <h2>Card</h2>
+        <p>Description</p>
+        <img width={'300px'} src="https://images.unsplash.com/photo-1576248004793-2c21b24b7bcc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80" alt="" />
+
+    </div>
+  )
+}
+
+export default Card
