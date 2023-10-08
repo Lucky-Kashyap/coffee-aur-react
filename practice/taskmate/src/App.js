80aa763bf41e074487851a02ccbb38a988a4d7d0
Lucky <luckykashyap3430@gmail.com>
2023-10-08 14:53:44 +0530

    syntax in class based component

--- practice/taskmate/src/App.js
+++ practice/taskmate/src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+// function App() {
+//   return (
+//     <div className="App">
       
-      <h1>Create react app</h1>
-    </div>
-  );
+//       <h1>Create react app</h1>
+//     </div>
+//   );
+// }
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div>App</div>
+    )
+  }
 }
+
 
 export default App;
