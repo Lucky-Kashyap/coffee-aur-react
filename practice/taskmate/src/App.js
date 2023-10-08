3b489ad6df850e0681dcd38e40ac1932ee01d187
Lucky <luckykashyap3430@gmail.com>
2023-10-08 22:46:12 +0530

    map through list of array

--- practice/taskmate/src/App.js
+++ practice/taskmate/src/App.js
@@ -5,45 +5,58 @@ import React, { useState } from "react";
 // import Footer from "./components/Footer";
 
 function App() {
+  const [tasks,setTasks]=useState([{id:5271,name:"Record Lectures",completed:false},{id:7825,name:"Edit React Lectures",completed:true},{id:8489,name:"Watch Lectures",completed:false}]);
   // let count=0;
-  const [count,setCount] = useState(0);
+  // const [count,setCount] = useState(0);
 
   // const username = "Divyanshu Kashyap";
 
-  const add = ()=>{
-    // count +=1;
-    // setCount(count+1);
-    // setCount(count+1);
-    // setCount(count+1);    // it holds the value
+  // const add = ()=>{
+  //   // count +=1;
+  //   // setCount(count+1);
+  //   // setCount(count+1);
+  //   // setCount(count+1);    // it holds the value
 
-    setCount(count=>count+1);
-    setCount(count=>count+1);
-    setCount(count=>count+1);
-    // console.log(count);
-  }
-  const sub = ()=>{
+  //   setCount(count=>count+1);
+  //   setCount(count=>count+1);
+  //   setCount(count=>count+1);
+  //   // console.log(count);
+  // }
+  // const sub = ()=>{
     
-    setCount(count-1);
-    console.log(count);
-  }
-  const reset = ()=>{
-    setCount(0);
-  }
+  //   setCount(count-1);
+  //   console.log(count);
+  // }
+  // const reset = ()=>{
+  //   setCount(0);
+  // }
   return (
     <div className="app">
       {/* <h1>Create react app </h1> */}
-      <div className="box">
+      {/* <div className="box">
         <p>{count}</p>
         <button onClick={add} className="add">Add</button>
         <button onClick={sub} className="sub">Sub</button>
         <button onClick={reset} className="sub">Reset</button>
-      </div>
+      </div> */}
 
       {/* <Header/>
       <Card/>
       <Footer/> */}
       {/* {React.createElement('h2',{},'Heading2')}
       <h3>Created By : {username}</h3> */}
+
+<h1>Task List</h1>
+<ul>
+  {
+    tasks.map(item=>(
+      <li key={item.id}>
+        <span>{item.id} - {item.name}</span>
+        <button>Delete</button>
+      </li>
+    ))
+  }
+</ul>
     </div>
   );
 }
