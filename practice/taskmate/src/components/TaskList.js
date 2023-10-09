8b8a90758baf3049c3e51a9df0f50cc80ef148e2
Lucky <luckykashyap3430@gmail.com>
2023-10-09 13:35:20 +0530

    inline level styling

--- practice/taskmate/src/components/TaskList.js
+++ practice/taskmate/src/components/TaskList.js
@@ -1,15 +1,22 @@
 import { useState } from "react";
 import TaskCard from "./TaskCard";
 import BoxCard from "./BoxCard";
-import './taskList.css';
+import "./taskList.css";
 
-export const TaskList = ({title,info}) => {
+export const TaskList = ({ title, info }) => {
   const [tasks, setTasks] = useState([
     { id: 5271, name: "Record React Lectures", completed: true },
     { id: 7825, name: "Edit React Lectures", completed: false },
     { id: 8391, name: "Watch Lectures", completed: false },
   ]);
   const [show, setShow] = useState(true);
+  const styles={
+    color:'#be3434',
+    border:'1px solid #be3434',
+    borderRadius:'5px',
+    fontSize:'20px',
+    padding:'20px'
+  }
 
   function handleDelete(id) {
     setTasks(tasks.filter((task) => task.id !== id));
@@ -17,7 +24,12 @@ export const TaskList = ({title,info}) => {
 
   return (
     <section className="tasklist">
-      <h1>Task List - {title}</h1>
+         <h1 style={styles}>
+        Task List - {title}
+      </h1>
+      {/* <h1 style={{ color: "#be3434", border: "1px solid #be3434" }}>
+        Task List - {title}
+      </h1> */}
       {/* <p>Info - {info}</p> */}
       <ul>
         <button className="trigger" onClick={() => setShow(!show)}>
@@ -25,7 +37,12 @@ export const TaskList = ({title,info}) => {
         </button>
         {show &&
           tasks.map((task) => (
-            <TaskCard info={info} key={task.id} task={task} handleDelete={handleDelete} />
+            <TaskCard
+              info={info}
+              key={task.id}
+              task={task}
+              handleDelete={handleDelete}
+            />
             // <li
             //   key={task.id}
             //   className={task.completed ? "completed" : "incomplete"}
@@ -39,26 +56,34 @@ export const TaskList = ({title,info}) => {
             // </li>
           ))}
       </ul>
-{/* <BoxCard res="success"/> */}
-{/* <BoxCard res="alert"/> */}
-{/* <BoxCard res="warning"/> */}
+      {/* <BoxCard res="success"/> */}
+      {/* <BoxCard res="alert"/> */}
+      {/* <BoxCard res="warning"/> */}
 
-<BoxCard res="success">
-            <p className="title">Lorem ipsum dolor sit amet.</p>
-            <p className="description">Lorem ipsum dolor sit amet adipisicing elit. Perferendis, vel!</p>
-        </BoxCard>
-        <BoxCard res="alert">
-            <p className="title">Lorem ipsum </p>
-            <p className="description">Lorem ipsum dolor, sit amet bore eaque furehenderit assumenda maxime, nam perferendis impedit libero odit eius eum aut cum ad, excepturi officiis repudiandae. Molestiae, eum cumque?</p>
-            {/* <p>Lorem dipisicing elit. Accusamus, nulla?</p> */}
-        </BoxCard>
+      <BoxCard res="success">
+        <p className="title">Lorem ipsum dolor sit amet.</p>
+        <p className="description">
+          Lorem ipsum dolor sit amet adipisicing elit. Perferendis, vel!
+        </p>
+      </BoxCard>
+      <BoxCard res="alert">
+        <p className="title">Lorem ipsum </p>
+        <p className="description">
+          Lorem ipsum dolor, sit amet bore eaque furehenderit assumenda maxime,
+          nam perferendis impedit libero odit eius eum aut cum ad, excepturi
+          officiis repudiandae. Molestiae, eum cumque?
+        </p>
+        {/* <p>Lorem dipisicing elit. Accusamus, nulla?</p> */}
+      </BoxCard>
 
-        <BoxCard res="warning">
-            <p className="title">Lorem ipsum dolor sit.</p>
-            <p className="description">Lorem ipsum dolor,  excepturi officiis repudiandae. Molestiae, eum cumque?</p>
-            {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, nulla?</p> */}
-        </BoxCard>
-     
+      <BoxCard res="warning">
+        <p className="title">Lorem ipsum dolor sit.</p>
+        <p className="description">
+          Lorem ipsum dolor, excepturi officiis repudiandae. Molestiae, eum
+          cumque?
+        </p>
+        {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, nulla?</p> */}
+      </BoxCard>
     </section>
   );
 };
