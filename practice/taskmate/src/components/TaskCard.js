d764a5f22b5152e5d4829013b99a89952b92b3e8
Lucky <luckykashyap3430@gmail.com>
2023-10-09 14:11:33 +0530

    module level styling

--- practice/taskmate/src/components/TaskCard.js
+++ practice/taskmate/src/components/TaskCard.js
@@ -1,11 +1,12 @@
 import './taskCard.css';
+import styles from './TaskCard.module.css';
 
 const TaskCard = ({ task, handleDelete,info }) => {
     return (
         <div className='taskCard'>
       <li className={`${task.completed ? "completed" : "incomplete"}`}>
         {/* <p>Info - {info}</p> */}
-          <span>{task.id} - {task.name} - {info}</span>
+          <span className={styles.title}>{task.id} - {task.name} - {info}</span>
           <button onClick={() => handleDelete(task.id)} className='delete'>Delete</button>
       </li>
       </div>
