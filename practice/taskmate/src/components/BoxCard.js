efba26d96b5cbd1e1d7207db54704767979d7e37
Lucky <luckykashyap3430@gmail.com>
2023-10-09 11:08:02 +0530

    props children

---
+++ practice/taskmate/src/components/BoxCard.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+
+const BoxCard = ({res,children})=>{
+    const [close,setClose]= useState(true);
+    return (
+        <div className={close ? "" : "hidden"}>
+        <div className={`box ${res}`}>
+            {children}
+            <button onClick={() => setClose(!close)} className="trigger">Close</button>
+        </div>
+    </div>
+    //     <>
+    //     <div className={`box ${res}`}>
+    //         {children}
+    //     {/* <p className="title">Lorem ipsum dolor sit amet consectetur </p> */}
+    //     {/* <p className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, vel!</p> */}
+
+    //   </div>
+    //   <button className="trigger" onClick={()=>setClose(!close)}>Close</button>
+    //   </>
+    )
+}
+
+export default BoxCard;
--- practice/taskmate/src/components/TaskList.js
+++ practice/taskmate/src/components/TaskList.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import TaskCard from "./TaskCard";
+import BoxCard from "./BoxCard";
 
 export const TaskList = ({title}) => {
   const [tasks, setTasks] = useState([
@@ -36,6 +37,26 @@ export const TaskList = ({title}) => {
             // </li>
           ))}
       </ul>
+{/* <BoxCard res="success"/> */}
+{/* <BoxCard res="alert"/> */}
+{/* <BoxCard res="warning"/> */}
+
+<BoxCard res="success">
+            <p className="title">Lorem ipsum dolor sit amet.</p>
+            <p className="description">Lorem ipsum dolor sit amet adipisicing elit. Perferendis, vel!</p>
+        </BoxCard>
+        <BoxCard res="alert">
+            <p className="title">Lorem ipsum </p>
+            <p className="description">Lorem ipsum dolor, sit amet bore eaque furehenderit assumenda maxime, nam perferendis impedit libero odit eius eum aut cum ad, excepturi officiis repudiandae. Molestiae, eum cumque?</p>
+            {/* <p>Lorem dipisicing elit. Accusamus, nulla?</p> */}
+        </BoxCard>
+
+        <BoxCard res="warning">
+            <p className="title">Lorem ipsum dolor sit.</p>
+            <p className="description">Lorem ipsum dolor,  excepturi officiis repudiandae. Molestiae, eum cumque?</p>
+            {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, nulla?</p> */}
+        </BoxCard>
+     
     </>
   );
 };
