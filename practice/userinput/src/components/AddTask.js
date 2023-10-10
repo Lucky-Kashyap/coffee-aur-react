f7980a7a50bcdaa5be7c52a7150a5e0390ac58f2
Lucky <luckykashyap3430@gmail.com>
2023-10-10 20:43:18 +0530

    controlled input

--- practice/userinput/src/components/AddTask.js
+++ practice/userinput/src/components/AddTask.js
@@ -4,14 +4,25 @@ import './AddTask.css';
 const AddTask = () => {
     const [taskValue,setTaskValue] = useState(""); 
 
+    const handleChange = (event) => {
+        setTaskValue(event.target.value);
+    }
+
+    const handleReset = () => {
+        setTaskValue("");
+    }
+
   return (
     <section className='addtask'>
         <form>
             {/* <label htmlFor="task">Task Name:</label> */}
-            <input type="text" onChange={(e) => setTaskValue(e.target.value)} name="task" id='task' autoComplete='off' placeholder='Task Name' />
+            {/* <input type="text" onChange={(e) => setTaskValue(e.target.value)} name="task" id='task' autoComplete='off' placeholder='Task Name' /> */}
+            <input type="text" value={taskValue} onChange={handleChange} name="task" id='task' autoComplete='off' placeholder='Task Name' />
+
             <button type="submit">Add Task</button>
+            <span onClick={handleReset} className='reset'>Reset</span>
         </form>
-        <p>{taskValue.length}</p> 
+        {/* <p>{taskValue.length}</p>  */}
     </section>
   )
 }
