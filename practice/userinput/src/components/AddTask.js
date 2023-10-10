9b64c900d850cd58c37226671b4b84fe61eab589
Lucky <luckykashyap3430@gmail.com>
2023-10-10 20:30:16 +0530

    get input data using onChange

--- practice/userinput/src/components/AddTask.js
+++ practice/userinput/src/components/AddTask.js
@@ -1,15 +1,17 @@
+import { useState } from 'react';
 import './AddTask.css';
 
 const AddTask = () => {
+    const [taskValue,setTaskValue] = useState(""); 
 
-    
   return (
     <section className='addtask'>
         <form>
-            <label htmlFor="task">Task Name:</label>
-            <input type="text" name="task" id='task' autoComplete='off' placeholder='Task Name' />
+            {/* <label htmlFor="task">Task Name:</label> */}
+            <input type="text" onChange={(e) => setTaskValue(e.target.value)} name="task" id='task' autoComplete='off' placeholder='Task Name' />
             <button type="submit">Add Task</button>
         </form>
+        <p>{taskValue.length}</p> 
     </section>
   )
 }
