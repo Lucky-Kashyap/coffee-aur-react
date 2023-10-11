f5e48998a10ffd9ddb0f7b1dc7f5ba54b3c4051b
Lucky <luckykashyap3430@gmail.com>
2023-10-11 10:10:42 +0530

    productlist display

--- practice/shopmate/src/components/ProductList.js
+++ practice/shopmate/src/components/ProductList.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 
 export const ProductList = () => {
     const [products, setProducts] = useState([]);
-    const [count,setCount] = useState(0);
+    // const [count,setCount] = useState(0);
 
     console.log(products);
 
@@ -22,18 +22,30 @@ export const ProductList = () => {
     },[]);
 
 
-    const incrementCount=()=>{
-        setCount(count+1);
-    }
+    // const incrementCount=()=>{
+    //     setCount(count+1);
+    // }
   return (
+    <section>
+        { products.map((product) => (
+            <div className="card" key={product.id}>
+                <p className="id">{product.id}</p>
+                <p className="name">{product.name}</p>
+                <p className="info">
+                    <span>${product.price}</span>
+                    <span className={product.in_stock ? "instock" : "unavailable"}>{product.in_stock ? "In Stock" : "Unavailable"}</span>
+                </p>
+            </div>            
+        )) }
+    </section>
     
-    <div>
-      {
-         console.log('Render')
-      }
-        <h2>ProductList</h2>
-        <p>{count}</p>
-        <button onClick={incrementCount}>Click Counter</button>
-    </div>
+    // <div>
+    //   {
+    //      console.log('Render')
+    //   }
+    //     <h2>ProductList</h2>
+    //     <p>{count}</p>
+    //     <button onClick={incrementCount}>Click Counter</button>
+    // </div>
   )
 }
