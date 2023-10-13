156666583d7e79700db2aaa5af7d62b54ae4fcce
Lucky <luckykashyap3430@gmail.com>
2023-10-13 18:10:03 +0530

    theme change background

--- practice/todoapp/src/App.js
+++ practice/todoapp/src/App.js
@@ -1,22 +1,34 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import AddTask from './components/AddTask';
-import Header from './components/Header';
-import ShowTask from './components/ShowTask';
+import { useEffect, useState } from "react";
+import "./App.css";
+import AddTask from "./components/AddTask";
+import Header from "./components/Header";
+import ShowTask from "./components/ShowTask";
 
 function App() {
-  const[tasks,setTasks] = useState(JSON.parse(localStorage.getItem('task')) || []);
-  const [task,setTask] = useState({});
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("task")) || []
+  );
+  const [task, setTask] = useState({});
 
   useEffect(() => {
     localStorage.setItem("task", JSON.stringify(tasks));
   }, [tasks]);
 
   return (
-    <div className='App'>
-     <Header/>
-     <AddTask tasks={tasks} setTasks={setTasks} task={task} setTask={setTask}/>
-     <ShowTask tasks={tasks} setTasks={setTasks} task={task} setTask={setTask}/>
+    <div className="App">
+      <Header />
+      <AddTask
+        tasks={tasks}
+        setTasks={setTasks}
+        task={task}
+        setTask={setTask}
+      />
+      <ShowTask
+        tasks={tasks}
+        setTasks={setTasks}
+        task={task}
+        setTask={setTask}
+      />
     </div>
   );
 }
--- practice/todoapp/src/components/Header.js
+++ practice/todoapp/src/components/Header.js
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from '../assets/general-task-list.png';
 
 const Header = () => {
-  return (
-    <header>
-    <div className="logo">
-        <img src={logo} alt="Taskmate Logo" />
-        <span>Taskmate</span>
-    </div>
-    <div className="themeSelector">
-        <span className="light"></span>
-        <span className="medium"></span>
-        <span className="dark activeTheme"></span>
-        <span className="blue"></span>
-        <span className="dark-blue"></span>
-        <span className="purple"></span>
-    </div>
-</header>
-  )
+    const [theme, setTheme] = useState(JSON.parse(localStorage.getItem("theme")) || "dark");
+
+    useEffect(() => {
+      localStorage.setItem("theme", JSON.stringify(theme));
+      document.documentElement.removeAttribute("class");
+      document.documentElement.classList.add(theme);
+    }, [theme]);
+  
+    return (
+      <header>
+          <div className="logo">
+              <img src={logo} alt="Todo Logo" />
+              <span>Taskmate</span>
+          </div>
+          <div className="themeSelector">
+              <span onClick={() => setTheme("light")} className={ theme === "light" ? "light activeTheme" : "light"}></span>
+              <span onClick={() => setTheme("medium")} className={ theme === "medium" ? "medium activeTheme" : "medium"}></span>
+              <span onClick={() => setTheme("dark")} className={ theme === "dark" ? "dark activeTheme" : "dark"}></span>
+              <span onClick={() => setTheme("blue")} className={ theme === "blue" ? "blue activeTheme" : "blue"}></span>
+              <span onClick={() => setTheme("dark-blue")} className={ theme === "dark-blue" ? "dark-blue activeTheme" : "dark-blue"}></span>
+              <span onClick={() => setTheme("purple")} className={ theme === "purple" ? "purple activeTheme" : "purple"}></span>
+          </div>
+      </header>
+    )
 }
 
 export default Header
