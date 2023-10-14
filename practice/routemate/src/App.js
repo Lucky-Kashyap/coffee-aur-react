34579b5973bae6ada776b5dd1eda88baa6e0376c
Lucky <luckykashyap3430@gmail.com>
2023-10-14 16:37:39 +0530

    no route match display page not found component

--- practice/routemate/src/App.js
+++ practice/routemate/src/App.js
@@ -7,6 +7,7 @@ import { Contact } from './components/Contact';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { Admin } from './components/Admin';
+import { PageNotFound } from './components/PageNotFound';
 
 function App() {
   const user = true;
@@ -20,12 +21,12 @@ function App() {
      <main>
      <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/products" element={<ProductList />}></Route>
-        <Route path="/products/:id" element={<ProductDetail />}></Route>
-        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="products" element={<ProductList />}></Route>
+        <Route path="products/:id" element={<ProductDetail />}></Route>
+        <Route path="contact" element={<Contact />}></Route>
         {/* <Route path='/admin' element={<Navigate to='/'/>}></Route> */}
-        <Route path='/admin' element={user?<Admin/>: <Navigate to='/'/>}></Route>
-
+        <Route path='admin' element={user?<Admin/>: <Navigate to='/'/>}></Route>
+        <Route path="*" element={<PageNotFound title="404"/>}></Route>
       </Routes>
      </main>
      
