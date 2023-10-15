a0300efa184084311054338311c884b7b9d3dd2e
Lucky <luckykashyap3430@gmail.com>
2023-10-15 14:39:37 +0530

    navbar ready

--- practice/tailwind css proj/js/index.js
+++ practice/tailwind css proj/js/index.js
@@ -1,20 +1,34 @@
+// tailwind.config = {
+//     darkMode: 'class',
+//     theme: {
+//         extend: {
+//             colors: {
+//                 "test" : {
+//                     100: '#cffafe',
+//                     200: '#a5f3fc',
+//                     300: '#67e8f9',
+//                     400: '#22d3ee',
+//                     500: '#06b6d4',
+//                     600: '#0891b2',
+//                     700: '#0e7490',
+//                     800: '#155e75',
+//                     900: '#164e63',
+//                   }
+//             }
+//         }
+//     }
+// }
+
 tailwind.config = {
-    darkMode: 'class',
-    theme: {
-        extend: {
-            colors: {
-                "test" : {
-                    100: '#cffafe',
-                    200: '#a5f3fc',
-                    300: '#67e8f9',
-                    400: '#22d3ee',
-                    500: '#06b6d4',
-                    600: '#0891b2',
-                    700: '#0e7490',
-                    800: '#155e75',
-                    900: '#164e63',
-                  }
-            }
-        }
-    }
-}
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        logo: {
+          700: "#1C59AE",
+          800: "#114a9a",
+        },
+      },
+    },
+  },
+};
