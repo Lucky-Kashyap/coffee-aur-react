960be1b70a86cd315be7df306d7fed1c1e225466
Lucky <luckykashyap3430@gmail.com>
2023-10-27 16:48:45 +0530

    configure routing

--- react-router/src/components/Footer/Footer.jsx
+++ react-router/src/components/Footer/Footer.jsx
@@ -20,8 +20,7 @@ export default function Footer() {
                             <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Resources</h2>
                             <ul className="text-gray-500 font-medium">
                                 <li className="mb-4">
-                                    <Link to="/" className={({isActive})=> `${isActive ? "text-orange-700" : "text-gray-700"} hover:underline
-                                `}>
+                                    <Link to="/" className="hover:underline">
                                         Home
                                     </Link>
                                 </li>
