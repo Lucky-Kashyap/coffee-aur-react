9411eaf35d16362af36f359d1c53e181383badb1
Lucky <luckykashyap3430@gmail.com>
2023-10-27 17:41:01 +0530

    add some style

--- react-router/src/components/Github/Github.jsx
+++ react-router/src/components/Github/Github.jsx
@@ -2,29 +2,29 @@ import React, { useEffect, useState } from 'react'
 import { useLoaderData } from 'react-router-dom'
 
 function Github() {
-    // const data = useLoaderData()
+    const data = useLoaderData()
 
 
-    const [data, setData] = useState([])
-    useEffect(() => {
-     fetch('https://api.github.com/users/Lucky-Kashyap')
-     .then(response => response.json())
-     .then(data => {
-        console.log(data);
-        setData(data)
-     })
-    }, [])
+    // const [data, setData] = useState([])
+    // useEffect(() => {
+    //  fetch('https://api.github.com/users/Lucky-Kashyap')
+    //  .then(response => response.json())
+    //  .then(data => {
+    //     console.log(data);
+    //     setData(data)
+    //  })
+    // }, [])
     
   return (
-        <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: {data?.followers}
-            <img src={data.avatar_url} alt="Git picture" width={300} />
+        <div className='flex justify-center items-center text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: {data?.followers}
+            <img className='w-80' src={data.avatar_url} alt="Git picture" width={300} />
     </div>
   )
 }
 
-export default Github
+export default Github;
 
-// export const githubInfoLoader = async () => {
-//     const response = await fetch('https://api.github.com/users/Lucky-Kashyap')
-//     return response.json()
-// }
+export const githubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/Lucky-Kashyap')
+    return response.json()
+}
