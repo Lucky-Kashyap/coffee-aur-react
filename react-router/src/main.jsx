3e9730a3399739fb76e0656310e7e8cec73343a2
Lucky <luckykashyap3430@gmail.com>
2023-10-27 18:37:40 +0530

    format code & add netlify.toml file for server config

--- react-router/src/main.jsx
+++ react-router/src/main.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter, Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
-import Layout from './components/Layout.jsx'
-import Home from './components/Home/Home.jsx'
-import Contact from './components/Contact/Contact.jsx'
-import About from './components/About/About.jsx'
-import Github,{githubInfoLoader} from './components/Github/Github.jsx'
-import User from './components/User/User.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import {
+  BrowserRouter,
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Layout from "./components/Layout.jsx";
+import Home from "./components/Home/Home.jsx";
+import Contact from "./components/Contact/Contact.jsx";
+import About from "./components/About/About.jsx";
+import Github, { githubInfoLoader } from "./components/Github/Github.jsx";
+import User from "./components/User/User.jsx";
 
 // const router = createBrowserRouter([
 //   {
@@ -40,25 +46,21 @@ import User from './components/User/User.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<Layout />}>
-      <Route path='' element={<Home />} />
-      <Route path='about' element={<About />} />
-      <Route path='contact' element={<Contact />} />
-      <Route path='user/:userid' element={<User />} />
-      <Route 
-      loader={githubInfoLoader}
-      path='github' 
-      element={<Github />}
-       />
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="user/:userid" element={<User />} />
+      <Route loader={githubInfoLoader} path="github" element={<Github />} />
     </Route>
   )
-)
+);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router}></RouterProvider>
     {/* <BrowserRouter> */}
     {/* <App /> */}
     {/* </BrowserRouter> */}
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
