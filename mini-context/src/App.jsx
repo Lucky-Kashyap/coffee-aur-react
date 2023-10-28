05f69b54680f03a519331114ecfd9a9867002d88
Lucky <luckykashyap3430@gmail.com>
2023-10-28 18:24:32 +0530

    config context

--- mini-context/src/App.jsx
+++ mini-context/src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
+import UserContextProvider from './context/UserContextProvider'
 
 function App() {
  
 
   return (
-    <>
+    <UserContextProvider>
       
       <h1>APP</h1>
-    </>
+    </UserContextProvider>
   )
 }
 
