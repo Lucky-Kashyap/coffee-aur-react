3ecda2e8998b469938128709d4870938e99db70a
Lucky <luckykashyap3430@gmail.com>
2023-10-29 11:47:32 +0530

    create context for theme

--- theme-toggler/src/App.jsx
+++ theme-toggler/src/App.jsx
@@ -1,12 +1,43 @@
+
+import { useEffect, useState } from 'react'
 import './App.css'
+import { ThemeProvider } from './context/theme'
+import ThemeToggle from './components/ThemeToggle'
+import Card from './components/Card'
 
 function App() {
+  const [themeMode, setThemeMode] = useState("light")
+
+  const lightTheme = () => {
+    setThemeMode("light")
+  }
+
+  const darkTheme = () => {
+    setThemeMode("dark")
+  }
+
+  // actual change in theme
+
+  useEffect(() => {
+    document.querySelector('html').classList.remove("light", "dark")
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
   
 
   return (
-    <>
-      <h1 className='bg-yellow-700'>Theme Toggler</h1>
-    </>
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+          <div className="w-full">
+              <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                  <ThemeToggle />
+              </div>
+
+              <div className="w-full max-w-sm mx-auto">
+                  <Card />
+              </div>
+          </div>
+      </div>
+    </ThemeProvider>
   )
 }
 
