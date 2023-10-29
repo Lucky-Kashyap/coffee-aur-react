ddc9185081dd10be40281992c20495635b0d6bf2
Lucky <luckykashyap3430@gmail.com>
2023-10-29 20:57:45 +0530

    character count functionality done

--- practice/word-counter/src/components/Counter.js
+++ practice/word-counter/src/components/Counter.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import { useRef, useState } from "react";
 
 const Counter = () => {
-  return (
-    <div>Counter</div>
-  )
-}
+    const [charLength,setCharLength] = useState(0);
+    const [wordLength,setWordLength] = useState(0);
 
+    const textInput = useRef('');
+
+
+    const handleInput=()=>{
+        const res = textInput.current.value;
+
+        setCharLength(res.length);
+
+
+
+    }
+    const removeData=()=>{
+        textInput.current.value='';
+        handleInput();
+    }
+
+  return (
+    <section className="counter">
+      <textarea ref={textInput}
+        data-testid="textArea"
+        placeholder="Type or paste your text" onChange={handleInput}
+      ></textarea>
+      <button onClick={removeData} data-testid="clearBtn">Clear</button>
+
+      <p className="result">
+        <span data-testid="charLength">Character: {charLength} </span>
+        <span data-testid="wordLength">Word: {wordLength}</span>
+      </p>
+    </section>
+  );
+};
 export default Counter;
