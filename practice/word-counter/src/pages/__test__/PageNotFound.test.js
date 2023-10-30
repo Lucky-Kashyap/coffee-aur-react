1f57dca7d74fd1c03cf886d3220a59d64f011eb1
Lucky <luckykashyap3430@gmail.com>
2023-10-30 17:42:33 +0530

    wrap it into BrowserRouter wrapper in pageNotFound testing file

--- practice/word-counter/src/pages/__test__/PageNotFound.test.js
+++ practice/word-counter/src/pages/__test__/PageNotFound.test.js
@@ -1,11 +1,20 @@
-import { render,screen } from "@testing-library/react"
+import { render} from "@testing-library/react"
 import PageNotFound from "../PageNotFound"
+import { BrowserRouter } from "react-router-dom";
 
 describe('Test PageNotFound Component',()=>{
     test('render text & Image',()=>{
-        render(<PageNotFound/>);
-        const page = screen.getByTestId('pnfText');
 
-        expect(page.innerHTML).toBe('Oops - Page Not Found!');
-    })
+        const { getByTestId, getByAltText } = render(<PageNotFound />, { wrapper: BrowserRouter });
+        const pnfText = getByTestId("pnfText");
+        const pnfImage = getByAltText("Page Not Found");
+        
+        expect(pnfText.innerHTML).toBe("Oops - Page Not Found!");
+        expect(pnfImage).toBeTruthy();
+    // });
+        // render(<PageNotFound/>);
+        // const page = screen.getByTestId('pnfText');
+
+        // expect(page.innerHTML).toBe('Oops - Page Not Found!');
+    });
 })
