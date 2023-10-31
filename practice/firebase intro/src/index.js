3c680cebc707c952b3836559cfc11f86ceca1411
Lucky <luckykashyap3430@gmail.com>
2023-10-31 12:28:47 +0530

    firebase connection

--- practice/firebase intro/src/index.js
+++ practice/firebase intro/src/index.js
@@ -1,1 +1,13 @@
-console.log('Firebase setup');
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDwHQtMWtq0iv_pW3ZIALe51FgOcQX6zo4",
+  authDomain: "notnotion-be816.firebaseapp.com",
+  projectId: "notnotion-be816",
+  storageBucket: "notnotion-be816.appspot.com",
+  messagingSenderId: "441036589173",
+  appId: "1:441036589173:web:846e216c1db21dccd7503c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
