3f71ec6cafaa895d557969e4251b824037bfe2c6
Lucky <luckykashyap3430@gmail.com>
2023-10-31 17:51:51 +0530

    firebase queries

--- practice/firebase intro/src/index.js
+++ practice/firebase intro/src/index.js
@@ -1,5 +1,13 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection,onSnapshot, getDocs,addDoc,deleteDoc,doc } from "firebase/firestore";
+import {
+  getFirestore,
+  collection,
+  onSnapshot,
+  getDocs,
+  addDoc,
+  deleteDoc,
+  doc,query,where
+} from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDwHQtMWtq0iv_pW3ZIALe51FgOcQX6zo4",
@@ -15,15 +23,17 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore();
 const colRef = collection(db, "movies");
 
-// getDocs(colRef)
-//   .then((data) => {
-//     // console.log(data.docs)
-//     let movies = [];
-//     data.docs.forEach((document) => {
-//       // console.log(document.data());
-//       // console.log(document.id);
-//       movies.push({ ...document.data(), id: document.id });
-//     });
+const qRef = query(colRef, where("category", "==", "drama"));
+
+getDocs(qRef)
+  .then((data) => {
+    // console.log(data.docs)
+    let movies = [];
+    data.docs.forEach((document) => {
+      // console.log(document.data());
+      // console.log(document.id);
+      movies.push({ ...document.data(), id: document.id });
+    });
 
     // let box = document.querySelector(".box");
     // console.log(movies);
@@ -31,49 +41,45 @@ const colRef = collection(db, "movies");
     // box.innerHTML = movies?.map(item=><h1>item.name</h1>);
 
     // setTimeout(() => {
-      
+
     //     box.innerHTML = movies[0].name;
     // }, 1000);
     // console.log(movies[0].name);
 
-  //   console.log(movies);
-  // })
-  // .catch((e) => console.log(e));
-
-   // snap shot call automatically
-
-   onSnapshot(colRef,(data)=>{
-    let movies = [];
-    // console.log('Changes');
-    data.docs.forEach(document=>{
-      movies.push({...document.data(), id: document.id});
-    })
-   })
-
-  const addForm = document.querySelector('.add');
-
-  addForm.addEventListener('submit',(e)=>{
-    e.preventDefault();
-    addDoc(colRef, {
-        name: addForm.name.value,
-        description: addForm.description.value
-    })
-    .then(() => {
-        addForm.reset();
-    });
+    console.log(movies);
   })
+  .catch((e) => console.log(e));
 
+// snap shot call automatically
 
- 
+//  onSnapshot(colRef,(data)=>{
+//   let movies = [];
+//   // console.log('Changes');
+//   data.docs.forEach(document=>{
+//     movies.push({...document.data(), id: document.id});
+//   })
+//  })
 
+const addForm = document.querySelector(".add");
+
+addForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  addDoc(colRef, {
+    name: addForm.name.value,
+    description: addForm.description.value,
+    category: addForm.category.value,
+  }).then(() => {
+    addForm.reset();
+  });
+});
 
 const deleteForm = document.querySelector(".delete");
 
-deleteForm.addEventListener("submit", event => {
-    event.preventDefault();
-    
-    const documentReference = doc(db, "movies", deleteForm.id.value);
-    deleteDoc(documentReference).then(() => {
-        deleteForm.reset();
-    });
+deleteForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const documentReference = doc(db, "movies", deleteForm.id.value);
+  deleteDoc(documentReference).then(() => {
+    deleteForm.reset();
+  });
 });
