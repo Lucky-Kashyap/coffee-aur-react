b74d65321e9580fa6704d5833f3d82d2338b4080
Lucky <luckykashyap3430@gmail.com>
2023-10-31 12:51:19 +0530

    fetch document from firebase

--- practice/firebase intro/src/index.js
+++ practice/firebase intro/src/index.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDwHQtMWtq0iv_pW3ZIALe51FgOcQX6zo4",
@@ -6,8 +7,19 @@ const firebaseConfig = {
   projectId: "notnotion-be816",
   storageBucket: "notnotion-be816.appspot.com",
   messagingSenderId: "441036589173",
-  appId: "1:441036589173:web:846e216c1db21dccd7503c"
+  appId: "1:441036589173:web:846e216c1db21dccd7503c",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore();
+const colRef = collection(db, "movies");
+
+getDocs(colRef).then((data) => {
+  // console.log(data.docs)
+
+  data.docs.forEach((document) => {
+    console.log(document.data());
+    console.log(document.id);
+  });
+});
