40c3e6c6df7f8144f21f96d78f0902571f953f9e
Lucky <luckykashyap3430@gmail.com>
2023-11-04 18:21:03 +0530

    UI design created

---
+++ practice/brand-page/src/components/Banner.jsx
@@ -0,0 +1,38 @@
+import shoe from '../assets/shoe_image.png';
+import amazon from '../assets/amazon.png';
+import flipkart from '../assets/flipkart.png';
+
+const Banner = () => {
+    return (
+      <main className="hero container">
+        <div className="hero-content">
+          <h1>YOUR FEET DESERVE THE BEST</h1>
+          <p>
+            YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+            SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+            SHOES.
+          </p>
+  
+          <div className="hero-btn">
+            <button>Shop Now </button>
+            <button className="secondary-btn">Category</button>
+          </div>
+  
+          <div className="shopping">
+            <p>Also Available On</p>
+  
+            <div className="brand-icons">
+              <img src={amazon} alt="amazon-logo" />
+              <img src={flipkart} alt="flipkart-logo" />
+            </div>
+          </div>
+        </div>
+        <div className="hero-image">
+          <img src={shoe} alt="hero-image" />
+        </div>
+      </main>
+    );
+  };
+  
+  export default Banner;
+  
